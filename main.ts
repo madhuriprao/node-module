b84230e8bf69b70387649d0f5e8d251cbd819285
madhuriprao <madhurirao014@gmail.com>
2024-09-24 03:12:43 +0200

    countChar function is added

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { reverseString } from "./stringUtilites"; // Remove the .ts extension
+import { countChar, reverseString } from "./stringUtilites"; // Remove the .ts extension
 
-console.log(reverseString('Checking the Reverse string custom module'));
+console.log(reverseString('Checking the Reverse string custom modules'));
+console.log(countChar(' Learning custome modules '))
 
--- stringUtilites.ts
+++ stringUtilites.ts
@@ -1,1 +1,6 @@
 export const reverseString = (str: string): string => str.split('').reverse().join('');
+
+export const countChar = (str:string):number => {
+return str.length;
+};
+
