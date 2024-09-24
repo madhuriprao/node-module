07aeaa7aceccf15235eb6f49276a40b49fd9595b
madhuriprao <madhurirao014@gmail.com>
2024-09-24 03:29:18 +0200

    vertical text function is added

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { countChar, reverseString } from "./stringUtilites"; // Remove the .ts extension
+import { countChar, reverseString, verticalText} from "./stringUtilites"; // Remove the .ts extension
 
-console.log(reverseString('Checking the Reverse string custom modules'));
-console.log(countChar(' Learning custome modules '))
-
+console.log(reverseString('Learning Node.js is important because it enables developers to build efficient server-side applications, like using modules to reverse strings, count characters, and display text vertically, enhancing both performance and productivity.'));
+console.log(countChar(' Learning Node.js is important because it enables developers to build efficient server-side applications, like using modules to reverse strings, count characters, and display text vertically, enhancing both performance and productivity. '));
+verticalText('Vertical text');
--- stringUtilites.ts
+++ stringUtilites.ts
@@ -1,6 +1,21 @@
+//creating a reverseStringfunction
+
 export const reverseString = (str: string): string => str.split('').reverse().join('');
 
+
+//countCharacter funtion
 export const countChar = (str:string):number => {
 return str.length;
 };
+
+//Vertical text funtion using for, of loop
+//with each for iteration, char holds the current character from the string.
+//of loop will go through each character of the string one by one. 
+export const verticalText = (str:string):void=>{
+    for (const char of str) {
+        console.log(char);
+}
+};
+
+
 
